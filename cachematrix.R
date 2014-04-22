12d84b0c829a6469f498aa230bcab6b6baf2970e
Peter Putz <putzpeter@yahoo.com>
2014-04-21 21:24:53 -0700

    Revert e4eed41..178cf51
    This rolls back to commit e4eed4153bd237a2dd4532fc3f40ce0e22fd0cb7.

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinverse <- function(inverse) inv <<- inverse
-    getinverse <- function() inv
-    list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)
+
 }
 
 
@@ -22,13 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    inv <- x$getinverse()
-    if(!is.null(inv)) {
-        message("retriEving cached inverse")
-        return(inv)
-    }
-    data <- x$get()
-    inverse <- solve(data, ...)
-    x$setinverse(inverse)
-    inverse
 }
